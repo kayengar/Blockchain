0d29fa8bac60d10a597bbcb3cfd8da6342ed6cb1
Karthik <kayengar@asu.edu>
2018-01-29 22:08:55 -0700

    Create Collections and database through endpoint

--- server.js
+++ server.js
@@ -1,44 +1,67 @@
 var express = require('express')
 var SHA256 = require("crypto-js/sha256");
-var mongoose = require("mongoose")
 var app = express()
 var blockChain = []
 var transactions = []
 
-var db = mongoose.connect('mongodb://localhost/blockchain');
 
-const TransactionSchema = new Schema({
+app.get('/addtransaction/:sender/:receiver/:amount/',function (request, response){
+var mongoose = require("mongoose")
+mongoose.Promise = global.Promise;
+var db = mongoose.connect('mongodb://localhost/Blockchain');
+
+mongoose.connection.on('connected', function () {  
+  console.log('Mongoose default connection open to ');
+}); 
+
+mongoose.connection.on('error',function (err) {  
+  console.log('Mongoose default connection error: ' + err);
+}); 
+
+// When the connection is disconnected
+mongoose.connection.on('disconnected', function () {  
+  console.log('Mongoose default connection disconnected'); 
+});
+
+const TransactionSchema = new mongoose.Schema({
 	sender : String,
 	receiver : String, 
 	amount : Number
 })
 
-const BlockchainSchema  = new Schema({
+const BlockchainSchema  = new mongoose.Schema({
 	index: Number,
 	timeStamp : Date,
 	data : TransactionSchema,
 	hash : String,
 })
 
-
-mongoose.model('BlockchainTable',BlockchainSchema)
-var BlockchainTable = mongoose.model('BlockchainTable')
-console.log('Here')
-lastBlock = BlockchainSchema.findOne().sort({timestamp:-1});
-
 mongoose.model('TransactionTable',TransactionSchema)
 var TransactionTable = mongoose.model('TransactionTable')
 
+// TransactionSchema.save()
 
-app.get('/addtransaction/:sender/:receiver/:amount/',function (request, response){
+
+
+mongoose.model('BlockchainTable',BlockchainSchema)
+var BlockchainTable = mongoose.model('BlockchainTable')
+
+
+
+var tt = new TransactionTable()
+
+// BlockchainSchema.save()
+console.log('Here')
+lastBlock = BlockchainTable.findOne().sort({timestamp:-1});
 	temp = []
 	temp.push(request.params.sender)
 	temp.push(request.params.receiver)
 	temp.push(request.params.amount)
 	transactions.push(temp)
-	// TransactionTable.create(temp)
+	tt.create(temp)
+	tt.save()
 	// response.send(SHA256(transactions[transactions.length-1]).toString())
-	response.send(lastBlock)
+	response.send(temp)
 })
 
 app.get('/getLastBlock/',function(){
